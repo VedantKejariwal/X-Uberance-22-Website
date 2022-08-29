5be1e9ba22d3949d28d2723cc00c3a0dbb6cf5b5
Shreevrind <109296410+Shreevrind@users.noreply.github.com>
2022-08-29 16:53:06 +0530

    Add files via upload

--- Schedule_Constants.dart
+++ Schedule_Constants.dart
@@ -1,12 +1,15 @@
 
-List Day1Event =['X-Enter','X-Log','X-Snap','X-Code','X-Goal','X-Pull','X-Hoop','X-Abhinoy','X-Hop','X-Calibre','X-Gaana','X-Pong','X-Drop','X-60','X-30','X-Vogue'];
+List Day1EventName =['X-Enter','X-Log','X-Snap','X-Code','X-Goal','X-Pull','X-Hoop','X-Abhinoy','X-Hop','X-Calibre','X-Gaana','X-Pong','X-Drop','X-60','X-30','X-Vogue'];
+List Day1Event =['Opening Ceremony','Girls Football','Boys Tug-O-War','Basket Ball','Bengali Play','dance Face Off','Debate Prelims','Antakshari','Table Tennis','Rap Battle/Beatboxing','One minute to fame','Half a Minute (Extempore)','Fashion Show'];
 List Day1Venue =['Main Auditorium','School Campus','School Campus','Computer Lab','Back Field','Primary School Field','Basket Ball Court','Main Auditorium','Gymnasium Podium','Reading Room','Xavier Hall','Games Room','Soumitra Sadan','Big Parlour','AV Room','Main Auditorium'];
 List Day1Timing =['11 AM - 12 PM','Full Day','Full Day','12:00','12:30','12:30','12:30','12:30','12:30','12:30','1:00','1:00','1:30','1:30','1:30','4:30'];
 
-List Day2Event =['X-Log','X-Snap','X-Goal','X-Wicket','X-Mate','X-Code','X-Splash','X-Pull','X-','X-Rhyme','X-Pun','X-Raga','X-Quizite','X-Trial','X-Acoustic'];
+List Day2EventName =['X-Log','X-Snap','X-Goal','X-Wicket','X-Mate','X-Code','X-Splash','X-Pull','X-','X-Rhyme','X-Pun','X-Raga','X-Quizite','X-Trial','X-Acoustic'];
+List Day2Event =['Vlogging','Photography','Boys Footbal','Box Cricket','Chess','Hackathon','Sketches & Spray Painting','Girls Tug-O-War','Fifa','Slam Poetry','Stand Up Comedy','Eastern Music','Quiz','Moot Court','Western Music'];
 List Day2Venue =['School Campus','School Campus','Back Field','Basket Ball Court','Big Parlour','Computer Lab','Games Room','Primary School Field','AV Room','Reading Room','Main Auditorium','Xavier Hall','Reading Room','AV Room','Main Auditorium'];
 List Day2Timing =['Full Day','Full Day','8:30','8:30','8:30','8:30','9:00','9:00','9:00','9:00','10:00','12:00','12:30','2:00','2:00'];
 
-List Day3Event =['X-Spike','X-Calibre','X-Bid','X-Act','X-Code','X-Hunt','X-Travagance','X-uent'];
+List Day3EventName =['X-Spike','X-Calibre','X-Bid','X-Act','X-Code','X-Hunt','X-Travagance','X-uent'];
+List Day3Event =['Volleyball','Debate','IPL Auction','AD Spoof','Hackathon','Treasure Hunt','Western Dance','Closing Ceremony'];
 List Day3Venue =['Back Field','Xavier Hall','Reading Room','AV Room','Computer Lab','School Campus','Main Auditorium','Main Auditorium'];
 List Day3Timing =['8:30','8:30','9:00','9:00','9 AM - 11 AM','10:00','2:30','6:00'];
